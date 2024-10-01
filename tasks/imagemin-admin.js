e0c8e7634c2d2d6e80b2c70880c2fc1d171aaf8a
augusto.oliveira <augusto.oliveira@coopersystem.com.br>
2024-10-01 13:37:51 -0300

    chore: Removed gulp files

--- tasks/imagemin-admin.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (gulp, plugins, config) {
-    gulp.task('imagemin-admin', function() {
-        return gulp.src(config.adminImageSrcFolder + "/**/*.{jpg,png,gif}")
-            .pipe(plugins.plumber())
-            .pipe(plugins.imagemin({ optimizationLevel: 5, progressive: true, interlaced: true }))
-            .pipe(gulp.dest(config.adminImageDestFolder))
-    });
-}
--- tasks/imagemin.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (gulp, plugins, config) {
-    gulp.task('imagemin', function() {
-		return gulp.src(config.imageSrcFolder + "/**/*.{jpg,png,gif}")
-			.pipe(plugins.plumber())
-    		.pipe(plugins.imagemin({ optimizationLevel: 5, progressive: true, interlaced: true }))
-    		.pipe(gulp.dest(config.imageDestFolder))
-    });
-};
